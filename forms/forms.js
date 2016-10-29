11f0cf93be588311855f57b88f59d59fc7f028cf
Brian Matney <brianamatney@gmail.com>
2016-10-29 18:58:48 -0500

    updated for base assignment

--- forms/forms.js
+++ forms/forms.js
@@ -1,36 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var array = [];
-    $('#employeeinfo').on('submit', function(event) {
-      event.preventDefault();
+    $('#employeeinfo').on('submit', function (event) {
+        event.preventDefault();
 
-      // initialize a new variable as an empty object
-      var values = {};
+        // initialize a new variable as an empty object
+        var values = {};
 
-      // convert the form inputs into an array
-      var fields = $('#employeeinfo').serializeArray();
+        // convert the form inputs into an array
+        var fields = $('#employeeinfo').serializeArray();
 
-      // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
-      fields.forEach(function(element, index, array) {
-        // review index notation vs. dot notation on objects
-        // here, dot notation wouldn't work
-        values[element.name] = element.value;
+        // iterate over the array and transfer each index into a new property on an object with the value of what was entered.
+        fields.forEach(function (element) {
+          values[element.name] = element.value;
+        });
+
+        console.log(values);
+
+        // clear out inputs
+        $('#employeeinfo').find('input[type=text]').val('');
+        // append to DOM
+        appendDom(values);
       });
-
-      console.log(values);
-
-      // clear out inputs
-      $('#employeeinfo').find('input[type=text]').val('');
-
-      // append to DOM
-      appendDom(values);
-    });
 
     function appendDom(empInfo) {
       $('#container').append('<div class="person"></div>');
       var $el = $('#container').children().last();
 
-      $el.append('<p>' + empInfo.employeefirstname + ' ' + empInfo.employeelastname + '</p>');
+      $el.append('<p>' + empInfo.employeeFirstName + '</p>');
+      $el.append('<p>' + empInfo.employeeLastName  + '</p>');
+      $el.append('<p>' + empInfo.employeeIdNumber + '</p>');
+      $el.append('<p>' + empInfo.employeeJobTitle + '</p>');
+      $el.append('<p>' + parseInt(empInfo.employeeSalary) + '</p>');
+
+      $('#monthlySalary').append('<div class="sal"></div>');
+      var salaries = $('#monthlySalary').children().last();
+      var salarySum = empInfo.employeeSalary / 12;
+
+      salaries.append('<p class="adjustedSalary">Month Salary Expenditures: $' + (salarySum + salarySum) + '</p>');
     }
-
-
-});
+  });
